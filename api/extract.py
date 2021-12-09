f09c3389dfc38d1e30df30c1e47c47127ff1c044
AkeKung <kingake555@gmail.com>
2021-12-09 14:14:33 +0700

    add levelshteinDistance

--- api/extract.py
+++ api/extract.py
@@ -25,6 +25,37 @@ def default(o):
     if isinstance(o, (date, datetime)):
         return o.strftime('%a, %d %b %Y 00:00:00 GMT')
 
+def levenshteinDistanceDP(token1, token2):
+        distances = np.zeros((len(token1) + 1, len(token2) + 1))
+
+        for t1 in range(len(token1) + 1):
+            distances[t1][0] = t1
+
+        for t2 in range(len(token2) + 1):
+            distances[0][t2] = t2
+            
+        a = 0
+        b = 0
+        c = 0
+        
+        for t1 in range(1, len(token1) + 1):
+            for t2 in range(1, len(token2) + 1):
+                if (token1[t1-1] == token2[t2-1]):
+                    distances[t1][t2] = distances[t1 - 1][t2 - 1]
+                else:
+                    a = distances[t1][t2 - 1]
+                    b = distances[t1 - 1][t2]
+                    c = distances[t1 - 1][t2 - 1]
+                    
+                    if (a <= b and a <= c):
+                        distances[t1][t2] = a + 1
+                    elif (b <= a and b <= c):
+                        distances[t1][t2] = b + 1
+                    else:
+                        distances[t1][t2] = c + 1
+        print(token1,token2)
+        return distances[len(token1)][len(token2)]
+
 class Extract(Resource):
 
     def __init__ (self):
@@ -146,15 +177,23 @@ class Extract(Resource):
     
     @classmethod
     def check_setting(cls,input,setting):
+        min=[100,""]
+        type=""
         for i,j in setting.items():
             for key in j:
                 if len(key) > len(input):
                     continue
-                if key in input:
-                    if input.strip().index(key)==0:
-                        return i,input.replace(key,"").strip()
-        return False,""
-
+                distance =levenshteinDistanceDP(input,key)
+                if(distance==0):
+                    return i,input.replace(key,"").strip()
+                elif(distance<min[0]):
+                    min=[distance,input.replace(key,"").strip()]
+                    type = i
+        if(min[0]<2):
+            return type,min[1]
+        else:
+            return False,""
+    
     def classify_head(self,head):
         sort_head=self.float_sort(head)
         h_state=''
